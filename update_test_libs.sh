3441e293247e2d576a8907e9e343c8f67570083e
Dante Pedrosa <dantepedrosa@gmail.com>
2025-11-22 19:42:51 -0300

    Refeito automação que prepara pastas de teste arduino

--- update_test_libs.sh
+++ update_test_libs.sh
@@ -1,33 +1,74 @@
 #!/bin/bash
 
-# Caminho base dos testes
-TEST_DIRS=(
-    "test/acquisition_test"
-    "test/encoder_test"
-    "test/motor_test"
-)
+# Pastas de teste
+TEST_DIR_ACQ="test/acquisition_test"
+TEST_DIR_ENCODER="test/encoder_test"
+TEST_DIR_MOTOR="test/motor_test"
 
-# Caminhos das pastas originais que serao copiadas
+# Origem das libs
 SRC_LIB="lib"
 SRC_INCLUDE="include"
 
-echo "Atualizando bibliotecas para pastas de teste..."
+echo "Atualizando bibliotecas para testes..."
 
-# Loop por cada pasta de teste
-for TEST_PATH in "${TEST_DIRS[@]}"; do
-    echo ""
-    echo "Processando: $TEST_PATH"
+######################################
+# 1) TESTE DE AQUISICAO (COMPLETO)
+######################################
+echo ""
+echo "[ACQUISITION TEST]"
 
-    # Remove pastas antigas de lib e include se existirem
-    rm -rf "$TEST_PATH/lib"
-    rm -rf "$TEST_PATH/include"
+# Limpa os arquivos antigos
+rm -f $TEST_DIR_ACQ/*.h
+rm -f $TEST_DIR_ACQ/*.cpp
 
-    # Copia as pastas novas
-    cp -r "$SRC_LIB" "$TEST_PATH/lib"
-    cp -r "$SRC_INCLUDE" "$TEST_PATH/include"
+# Copia config.h
+cp $SRC_INCLUDE/config.h $TEST_DIR_ACQ/
 
-    echo "Copiado lib/ e include/ para $TEST_PATH"
-done
+# Copia data_logger
+cp $SRC_LIB/data_logger/data_logger.h $TEST_DIR_ACQ/
+cp $SRC_LIB/data_logger/data_logger.cpp $TEST_DIR_ACQ/
+
+# Copia encoder
+cp $SRC_LIB/encoder/encoder.h $TEST_DIR_ACQ/
+cp $SRC_LIB/encoder/encoder.cpp $TEST_DIR_ACQ/
+
+# Copia motor
+cp $SRC_LIB/motor/motor.h $TEST_DIR_ACQ/
+cp $SRC_LIB/motor/motor.cpp $TEST_DIR_ACQ/
+
+echo "Arquivos copiados para $TEST_DIR_ACQ"
+
+
+######################################
+# 2) ENCODER TEST (SO ENCODER + CONFIG)
+######################################
+echo ""
+echo "[ENCODER TEST]"
+
+rm -f $TEST_DIR_ENCODER/*.h
+rm -f $TEST_DIR_ENCODER/*.cpp
+
+cp $SRC_INCLUDE/config.h $TEST_DIR_ENCODER/
+cp $SRC_LIB/encoder/encoder.h $TEST_DIR_ENCODER/
+cp $SRC_LIB/encoder/encoder.cpp $TEST_DIR_ENCODER/
+
+echo "Arquivos copiados para $TEST_DIR_ENCODER"
+
+
+######################################
+# 3) MOTOR TEST (SO MOTOR + CONFIG)
+######################################
+echo ""
+echo "[MOTOR TEST]"
+
+rm -f $TEST_DIR_MOTOR/*.h
+rm -f $TEST_DIR_MOTOR/*.cpp
+
+cp $SRC_INCLUDE/config.h $TEST_DIR_MOTOR/
+cp $SRC_LIB/motor/motor.h $TEST_DIR_MOTOR/
+cp $SRC_LIB/motor/motor.cpp $TEST_DIR_MOTOR/
+
+echo "Arquivos copiados para $TEST_DIR_MOTOR"
 
 echo ""
-echo "Finalizado!"
+echo "Finalizado com sucesso!"
